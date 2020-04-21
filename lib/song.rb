8ddb7ea6ed41b32059f1a19b38de667235481b37
Megan Comeau <mlcomeau99@gmail.com>
2020-04-21 19:59:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,12 +34,12 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) ==nil 
+    if self.find_by_name(name) ==nil
       self.create_by_name(name)
-    else 
+    else
       self.find_by_name(name)
     end
-    
+
   end
 
 
