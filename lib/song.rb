c520941dee186b895e8aa94737a9f33c4a3ffe70
Megan Comeau <mlcomeau99@gmail.com>
2020-04-21 20:14:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,14 +48,9 @@ class Song
   end
 
   def self.new_from_filename (filename)
-    data = filename.split(/[-.]/)
-    name = data[1]
-    artist_name = data[0]
-
-    song = self.new
-    song.name = name
-    song.artist_name = artist_name
-
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
     song
 
   end
