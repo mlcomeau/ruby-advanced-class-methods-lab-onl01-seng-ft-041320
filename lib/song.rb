f8adfc8cebdf16f234ded02d31e1f9d3d56803c2
Megan Comeau <mlcomeau99@gmail.com>
2020-04-21 19:41:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,5 +21,17 @@ class Song
     song.name = name
   end
 
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    @@all << song 
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  
+
 
 end
